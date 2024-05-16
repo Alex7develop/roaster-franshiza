078a1caeb6de903ef79a42a79355ca0d2dd13746
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2024-05-16 12:48:49 +0300

    reales0

--- js/script.js
+++ js/script.js
@@ -259,3 +259,23 @@ $(function () {
 });
 
 console.log('%CWOTTTTT', 'color: teal; font-size: 46px;');
+
+let mybutton = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+}
